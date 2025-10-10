dee893c336b3b7574bf0ab3cc653bc6115a81cf1
NitsGameDev <nitingamedev2921@gmail.com>
2025-10-10 20:49:38 +0530

    First

---
+++ script.js
@@ -0,0 +1,7 @@
+// A code confirm karse ke JavaScript file barabar link thai gai chhe.
+console.log("JavaScript successfully loaded! Tamaro Portfolio taiyar chhe.");
+
+// Tame ahiya navi features jode sako chho, jem ke:
+// 1. Smooth scrolling effects
+// 2. Button par click karva thi event
+// 3. Form validation
